5be3782d6cdde652a9aaadd48d0c300798e31faf
Homa Wong <homawong@gmail.com>
2016-11-09 12:18:52 -0800

    Update flux-standard-action.d.ts
    type can be `symbol`
    
    Adding comments as in `npm-flux-standard-action`

--- flux-standard-action/flux-standard-action.d.ts
+++ flux-standard-action/flux-standard-action.d.ts
@@ -9,8 +9,20 @@ declare module "flux-standard-action" {
     }
 
     export interface Action<T> {
-        type: string;
+        /**
+         * The type of an action identifies to the consumer the nature of the action that has occurred. Two actions with the same type MUST be strictly equivalent (using ===)
+         */
+        type: string | symbol;
+        /**
+         * The optional payload property MAY be any type of value. It represents the payload of the action. Any information about the action that is not the type or status of the action should be part of the payload field.
+         * By convention, if error is true, the payload SHOULD be an error object. This is akin to rejecting a promise with an error object.
+         */
         payload?: T;
+        /**
+         * The optional error property MAY be set to true if the action represents an error.
+         * An action whose error is true is analogous to a rejected Promise. By convention, the payload SHOULD be an error object.
+         * If error has any other value besides true, including undefined and null, the action MUST NOT be interpreted as an error.
+         */
         error?: boolean;
     }
 
@@ -19,7 +31,7 @@ declare module "flux-standard-action" {
         meta: any
     }
 
-    // Usage: var action: Action<sring> & TypedMeta<string>;
+    // Usage: var action: Action<string> & TypedMeta<string>;
     export interface TypedMeta<T> {
         meta: T
     }
